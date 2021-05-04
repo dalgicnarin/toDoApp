1943202500b421be64deabc62a6e36140905ef04
dalgicnarin <narindalgic@gmail.com>
2021-05-04 21:48:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// UI vars 
+
 
 const form = document.querySelector('form');
 const input = document.querySelector('#txtTaskName');
@@ -6,20 +6,16 @@ const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
 let items;
 
-// load items
+
 loadItems();
 
-// call event listeners
+
 eventListeners();
 
 function eventListeners() {
-    // submit event
+
     form.addEventListener('submit', addNewItem);
-
-    // delete an item
     taskList.addEventListener('click', deleteItem);
-
-    // delete all items
     btnDeleteAll.addEventListener('click', deleteAllItems);
 }
 
@@ -30,7 +26,6 @@ function loadItems() {
     });
 }
 
-// get items from Local Storage
 function getItemsFromLS(){
     if(localStorage.getItem('items')===null){
         items = [];
@@ -40,14 +35,12 @@ function getItemsFromLS(){
     return items;
 }
 
-// set item to Local Storage
 function setItemToLS(text){
     items = getItemsFromLS();
     items.push(text);
     localStorage.setItem('items',JSON.stringify(items));
 }
 
-// delete item from LS
 function deleteItemFromLS(text){
     items = getItemsFromLS();
     items.forEach(function(item,index){
@@ -60,62 +53,62 @@ function deleteItemFromLS(text){
 
 
 function createItem(text) {
-    // create li
+
     const li = document.createElement('li');
     li.className = 'list-group-item list-group-item-secondary';
     li.appendChild(document.createTextNode(text));
 
-    // create a
+
     const a = document.createElement('a');
     a.classList = 'delete-item float-right';
     a.setAttribute('href', '#');
     a.innerHTML = '<i class="fas fa-times"></i>';
 
-    // add a to li
+
     li.appendChild(a);
 
-    // add li to ul
+    
     taskList.appendChild(li);
 
 }
 
-// add new item
+
 function addNewItem(e) {
     if (input.value === '') {
         alert('add new item');
     }
 
-    // create item
+  
     createItem(input.value);
 
-    // save to LS
+  
     setItemToLS(input.value);
 
-    // clear input
+    
     input.value = '';
 
     e.preventDefault();
 
 }
 
-// delete an item
+
 function deleteItem(e) {
     if (e.target.className === 'fas fa-times') {
         if (confirm('Are you sure to remove your task?')) {
             e.target.parentElement.parentElement.remove();
 
-            // delete item from LS
+            
             deleteItemFromLS(e.target.parentElement.parentElement.textContent);
         }
     }
     e.preventDefault();
 }
 
-// delete all items
+
 function deleteAllItems(e) {
 
     if (confirm('Are you sure to remove all your task?')) {
-        // taskList.innerHTML='';
+        
         while(taskList.firstChild){
             taskList.removeChild(taskList.firstChild);
         }
